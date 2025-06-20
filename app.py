0289b9ab0e7697a5ff3771668441137987fccb93
olh0 <olh1289@outlook.com>
2025-06-20 11:38:00 +0800

    refine code

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ import datetime
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # 用于会话安全
+app.secret_key = 'jjfjjmldhzbwjzswwntx'  # 用于会话安全
 
-# 使用一个全局变量来模拟剪贴板数据库
+time_iso = datetime.datetime.now().isoformat()
+# 列表模拟剪贴板数据库
 # clip_list 存储的是 (内容, ISO格式时间字符串) 的元组
 clip_list = [
-    ('这是一个简单的网络剪贴板', datetime.datetime.now().isoformat()),
-    ('https://www.google.com', datetime.datetime.now().isoformat()),
-    ('第二条记录\n支持换行哦！', datetime.datetime.now().isoformat())
-] * 6 # 增加一些初始数据
+    ('这是一个简单的网络剪贴板', time_iso),
+    ('https://www.google.com', time_iso),
+    ('数据保存在服务器，勿轻易上传隐私信息！', time_iso),
+    ('若非要添加隐私信息，请及时清理！', time_iso),
+] * 2 # 初始数据
 
 @app.route('/', methods=['GET'])
 def index():
-    # 返回最新的6条记录，这些记录是元组列表
-    items = clip_list[::-1][0:6]
-    return render_template('index.html', items=items)
+    return render_template('index.html')
 
 @app.route('/add-item', methods=['POST'])
 def add_item():
@@ -28,17 +28,16 @@ def add_item():
     current_timestamp = request.form.get('timestamp')
 
     if new_item_content and new_item_content.strip():
-        # 后端生成时间戳，并使用ISO格式，方便前端解析
-        # current_timestamp = datetime.datetime.now().isoformat()
         new_item_tuple = (new_item_content.strip(), current_timestamp)
         clip_list.append(new_item_tuple)
-        logging.info(f"添加了新项目: {new_item_tuple[0]}，时间: {new_item_tuple[1]}")
+        logging.info(f"添加了新内容: {new_item_tuple[0]}，时间: {new_item_tuple[1]}")
 
         # 返回成功状态和更新后的列表
         return jsonify({
             'status': 'success',
             'message': '已添加！',
-            'items': clip_list[::-1][0:6] # 确保这里返回的是元组列表
+            'items': clip_list[::-1]
+            # 'items': clip_list[::-1][0:6] #限制列表数量
         })
     else:
         # 如果输入无效，返回错误状态
@@ -47,13 +46,14 @@ def add_item():
             'message': '请输入有效的内容'
         }), 400
 
-# 用于JS初始加载列表。前端暂用Jinja2方式获取列表
+# 用于JS初始加载列表。
 
 @app.route('/get-items', methods=['GET'])
 def get_items():
     return jsonify({
         'status': 'success',
-        'items': clip_list[::-1][0:6]
+        # 'items': clip_list[::-1][0:6]
+        'items': clip_list[::-1]
     })
 
 @app.route('/clear-all', methods=['POST'])
@@ -62,15 +62,15 @@ def clear_all():
     处理清除所有列表项的请求。
     """
     clip_list.clear() # 清空列表
-    logging.info("所有项目已被清除。")
+    logging.info("所有内容已被清除。")
 
     # 返回成功状态和空的列表
     return jsonify({
         'status': 'success',
-        'message': '所有项目已清除',
+        'message': '所有内容已清除',
         'items': [] # 清空后返回空列表
     })
 
 if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0', port='5001')
     app.run(debug=True)
-
